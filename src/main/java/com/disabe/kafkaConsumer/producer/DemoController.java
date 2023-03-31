c0854bf808c019b5123c3ee4e208cc31891852f2
disabe2005 <didiersabecheun@gmail.com>
2023-03-31 11:39:06 +0200

    prometheus add

--- src/main/java/com/disabe/kafkaConsumer/producer/DemoController.java
+++ src/main/java/com/disabe/kafkaConsumer/producer/DemoController.java
@@ -1,6 +1,8 @@
 package com.disabe.kafkaConsumer.producer;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-
+	 //final static Logger logger = (Logger) LoggerFactory.getLogger(DemoController.class);
 	 // Autowiring Kafka Template
     @Autowired 
     KafkaTemplate<String, String> kafkaTemplate;
@@ -24,6 +26,12 @@ public class DemoController {
         kafkaTemplate.send(TOPIC, message);
  
         return "Published Successfully";
+    }
+    
+    @GetMapping("/something")
+    public ResponseEntity<String> createLogs() {
+        //logger.warn("Just checking");
+        return ResponseEntity.ok().body("All Ok");
     }
 	
 }
